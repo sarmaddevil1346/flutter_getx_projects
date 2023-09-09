015441f64b1c92ad3deb3bd3c0190c9e434dc552
Muhammad Sarmad Nadeem <122588901+sarmaddevil1346@users.noreply.github.com>
2023-09-09 11:32:42 +0500

    GetMaterialApp and snackbar practice

--- studioprojects/lib/main.dart
+++ studioprojects/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:studioprojects/views/Screens.dart';
 
 void main() {
@@ -8,13 +9,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
--- studioprojects/lib/views/Screens.dart
+++ studioprojects/lib/views/Screens.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
 class IntroGetX extends StatelessWidget {
   const IntroGetX({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: getXSnackBar,
+      ),
+    );
+  }
+
+  void getXSnackBar() {
+    Get.snackbar("This is a label", "This is a message",
+        animationDuration: const Duration(seconds: 1),
+        backgroundColor: Colors.red,
+        snackPosition: SnackPosition.BOTTOM);
   }
 }
