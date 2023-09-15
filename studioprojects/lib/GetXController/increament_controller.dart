918e9a0ea9bd5f05c392d2120c1361d06e410514
Muhammad Sarmad Nadeem <122588901+sarmaddevil1346@users.noreply.github.com>
2023-09-15 08:47:18 +0500

    GetXCounter
    From here State Management of GetX start
     Counter Example

---
+++ studioprojects/lib/GetXController/increament_controller.dart
@@ -0,0 +1,10 @@
+import 'package:get/get.dart';
+class IncrementController extends GetxController{
+
+  final counter  =  1.obs;
+  increment()
+  {
+    counter
+  }
+
+}
---
+++ studioprojects/lib/views/components/default_snack_bar.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void getXSnackBar() {
+  Get.snackbar(
+    "This is a label",
+    "This is a message",
+    animationDuration: const Duration(seconds: 1),
+    backgroundColor: Colors.red,
+    snackPosition: SnackPosition.TOP,
+  );
+}
